a01f495f7c8a546b81495ef402a53d668eb56da2
seneshMH <senesh1senesh@gmail.com>
2022-08-28 21:10:12 +0530

    Add logging suport

--- Raptor/Raptor/src/Raptor/EntryPoint.h
+++ Raptor/Raptor/src/Raptor/EntryPoint.h
@@ -5,6 +5,9 @@
 extern Raptor::Application* Raptor::CreateApplication();
 
 int main(int argc,char** argv) {
+	Raptor::Log::Init();
+	RT_CORE_WARN("Initalize Log");
+	RT_INFO("Hello world");
 	
 	auto app = Raptor::CreateApplication();
 	app->Run();
