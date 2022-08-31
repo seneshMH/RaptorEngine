401c1370c90d5a0cf2ae42d1221fbe999ce17c78
seneshMH <senesh1senesh@gmail.com>
2022-08-31 23:11:19 +0530

    Fixing premake post build

--- Raptor/premake5.lua
+++ Raptor/premake5.lua
@@ -68,7 +68,7 @@ workspace "Raptor"
 
             postbuildcommands
             {
-                ("{COPY} %{cfg.buildtarget.relpath} ../bin/"..outputdir.."/Sandbox")
+                ("{COPY} %{cfg.buildtarget.relpath} \"../bin/"..outputdir.."/Sandbox/\"")
             }
 
         filter "configurations:Debug"
