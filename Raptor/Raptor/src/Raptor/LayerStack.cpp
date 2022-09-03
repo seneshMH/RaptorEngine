51da4c03a9e95444a75f96fb267cc01c33cf8030
seneshMH <senesh1senesh@gmail.com>
2022-09-03 13:07:30 +0530

    fix OnDetach call

--- Raptor/Raptor/src/Raptor/LayerStack.cpp
+++ Raptor/Raptor/src/Raptor/LayerStack.cpp
@@ -33,9 +33,9 @@ namespace Raptor{
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if(it != m_Layers.end())
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
-			layer->OnDetach();
 		}
 	}
 
@@ -44,8 +44,8 @@ namespace Raptor{
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
-			m_Layers.erase(it);
 			overlay->OnDetach();
+			m_Layers.erase(it);
 		}
 	}
 
