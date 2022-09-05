249fe93d9ac26e653bca3d7caf694d14b26ff3e1
seneshMH <senesh1senesh@gmail.com>
2022-09-05 11:47:14 +0530

    Moveing to sandbox and adding camera movement

--- Raptor/Raptor/src/Raptor.h
+++ Raptor/Raptor/src/Raptor.h
@@ -5,11 +5,16 @@
 #include "Raptor/Layer.h"
 #include "Raptor/ImGui/ImGuiLayer.h"
 
-
 #include "Raptor/Input.h"
 #include "Raptor/MouseButtonCodes.h"
 #include "Raptor/KeyCodes.h"
 
+#include "Raptor/Renderer/Renderer.h"
+#include "Raptor/Renderer/RenderCommand.h"
+#include "Raptor/Renderer/Buffer.h"
+#include "Raptor/Renderer/Shader.h"
+#include "Raptor/Renderer/VertexArray.h"
+#include "Raptor/Renderer/OrthographicCamera.h"
 
 //Entry Point
 #include "Raptor/EntryPoint.h"
--- Raptor/Raptor/src/Raptor/Application.cpp
+++ Raptor/Raptor/src/Raptor/Application.cpp
@@ -12,7 +12,6 @@ namespace Raptor {
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
-		:m_Camera(-1.6f,1.6f,-0.9f,0.9f)
 	{
 		RT_CORE_ASSERT(!s_Instance, "Application already exists");
 		s_Instance = this;
@@ -22,129 +21,6 @@ namespace Raptor {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
-
-		m_VertexArray.reset(VertexArray::Create());
-
-		float vertices[3 * 7] = {
-			-0.5f,-0.5f, 0.0f, 0.8f,0.2f,0.2f,1.0f,
-			 0.5f,-0.5f, 0.0f, 0.2f,0.8f,0.2f,1.0f,
-			 0.0f, 0.5f, 0.0f, 0.2f,0.2f,0.8f,1.0f
-		};
-		std::shared_ptr<VertexBuffer> vertexBuffer;
-		vertexBuffer.reset(VertexBuffer::Create(vertices,sizeof(vertices)));
-		
-		
-		BufferLayout layout = {
-			{ShaderDataType::Float3,"a_Position"},
-			{ShaderDataType::Float4,"a_Color"}
-		};
-
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddvertexBuffer(vertexBuffer);
-
-		unsigned int indices[3] = {0,1,2};
-
-		std::shared_ptr<IndexBuffer> indexBuffer;
-		indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
-
-		m_SqureVA.reset(VertexArray::Create());
-
-		float squreVertices[3 * 4] = {
-			-0.5f,-0.5f, 0.0f, 
-			 0.5f,-0.5f, 0.0f, 
-			 0.5f, 0.5f, 0.0f,
-			 -0.5f, 0.5f, 0.0f
-		};
-
-		std::shared_ptr<VertexBuffer> squreVB;
-		squreVB.reset(VertexBuffer::Create(squreVertices, sizeof(squreVertices)));
-
-		BufferLayout squreVBLayout = {
-			{ShaderDataType::Float3,"a_Position"},
-		};
-
-		squreVB->SetLayout(squreVBLayout);
-
-		m_SqureVA->AddvertexBuffer(squreVB);
-
-
-		unsigned int squreIndices[6] = { 0,1,2,2,3,0 };
-
-		std::shared_ptr<IndexBuffer> squreIB;
-		squreIB.reset(IndexBuffer::Create(squreIndices, sizeof(squreIndices) / sizeof(uint32_t)));
-
-		m_SqureVA->SetIndexBuffer(squreIB);
-
-		std::string vertexSrc = R"(
-			#version 330 core
-			
-			layout(location=0) in vec3 aPosition;
-			layout(location=1) in vec4 aColor;
-
-			uniform mat4 u_ViewProjection;
-
-			out vec3 vPosition;
-			out vec4 vColor;
-			
-			void main()
-			{
-				vPosition = aPosition;
-				vColor = aColor;
-				gl_Position = u_ViewProjection * vec4(aPosition,1.0);
-			}		
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-			
-			layout(location=0) out vec4 color;
-
-			in vec3 vPosition;
-			in vec4 vColor;
-			
-			void main()
-			{
-				//color = vec4(vPosition * 0.5 + 0.5,1.0);
-				color = vColor;
-			}		
-		)";
-
-
-		m_Shader.reset(new Shader(vertexSrc,fragmentSrc));
-
-
-		std::string BlueShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location=0) in vec3 aPosition;
-
-			uniform mat4 u_ViewProjection;
-
-			out vec3 vPosition;
-			
-			void main()
-			{
-				vPosition = aPosition;
-				gl_Position = u_ViewProjection * vec4(aPosition,1.0);
-			}		
-		)";
-
-		std::string BlueShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location=0) out vec4 color;
-
-			in vec3 vPosition;
-			
-			
-			void main()
-			{
-				color = vec4(0.2,0.3,0.8,1.0);
-			}		
-		)";
-
-		m_BlueShader.reset(new Shader(BlueShaderVertexSrc, BlueShaderFragmentSrc));
 	}
 	Application::~Application()
 	{
@@ -153,18 +29,6 @@ namespace Raptor {
 	{
 		while (m_Running) 
 		{
-			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-			RenderCommand::Clear();
-
-			m_Camera.SetRotation(45.0f);
-
-			Renderer::BeginScene(m_Camera);
-
-			Renderer::Submit(m_BlueShader,m_SqureVA);
-			Renderer::Submit(m_Shader,m_VertexArray);
-
-			Renderer::EndScene();
-
 			for (Layer* layer : m_LayerStack)
 			{
 				layer->OnUpdate();
