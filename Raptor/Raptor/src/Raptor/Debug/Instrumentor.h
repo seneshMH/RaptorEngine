28e5ca451e698d54ff708c8a1bc8a4f67ed0e71e
seneshMH <senesh1senesh@gmail.com>
2022-09-08 23:50:11 +0530

    Auto deducing an availabe __FUNCSIG__ definition

--- Raptor/Raptor/src/Raptor/Debug/Instrumentor.h
+++ Raptor/Raptor/src/Raptor/Debug/Instrumentor.h
@@ -122,12 +122,31 @@ namespace Raptor {
 	};
 }
 
-#define RT_PROFILE 0
+#define RT_PROFILE 1
+
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+	#define RT_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+	#define RT_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__FUNCSIG__)
+	#define RT_FUNC_SIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+	#define RT_FUNC_SIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+	#define RT_FUNC_SIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+	#define RT_FUNC_SIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+	#define RT_FUNC_SIG __func__
+#else
+#define RT_FUNC_SIG "RT_FUNC_SIG unknown!"
+#endif
+
 #if RT_PROFILE
 #define RT_PROFILE_BEGIN_SESSION(name, filepath) ::Raptor::Instrumentor::Get().BeginSession(name, filepath)
 #define RT_PROFILE_END_SESSION() ::Raptor::Instrumentor::Get().EndSession()
 #define RT_PROFILE_SCOPE(name) ::Raptor::InstrumentationTimer timer##__LINE__(name);
-#define RT_PROFILE_FUNCTION() RT_PROFILE_SCOPE(__FUNCSIG__)
+#define RT_PROFILE_FUNCTION() RT_PROFILE_SCOPE(RT_FUNC_SIG)
 #else
 #define RT_PROFILE_BEGIN_SESSION(name, filepath)
 #define RT_PROFILE_END_SESSION()
