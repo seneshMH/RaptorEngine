9e484202310da3be1580a6537f7c04e2476adfc8
seneshMH <senesh1senesh@gmail.com>
2022-09-08 10:39:52 +0530

    Fix Build warnings in BufferElement Offset Type

--- Raptor/Raptor/src/Platform/OpenGl/OpenGLVertexArray.cpp
+++ Raptor/Raptor/src/Platform/OpenGl/OpenGLVertexArray.cpp
@@ -73,7 +73,7 @@ namespace Raptor {
 				ShaderDataTypeToOpenGLDataType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				layout.GetStride(),
-				(const void*)(intptr_t)element.Offset
+				(const void*)element.Offset
 			);
 			index++;
 		}
