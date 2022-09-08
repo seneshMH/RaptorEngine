d4a593abe67c19ad410dc8b1c1b0d3890e45892b
seneshMH <senesh1senesh@gmail.com>
2022-09-08 10:19:59 +0530

    Use Single Shader for 2D Renderer

--- Raptor/Raptor/src/Platform/OpenGl/OpenGLTexture.cpp
+++ Raptor/Raptor/src/Platform/OpenGl/OpenGLTexture.cpp
@@ -5,6 +5,23 @@
 #include <stb_image.h>
 
 namespace Raptor {
+
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		:m_Width(width),m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Height, m_Width);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& filepath)
 		:m_Path(filepath)
 	{
@@ -29,6 +46,9 @@ namespace Raptor {
 			dataFormat = GL_RGB;
 		}
 
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
+
 		RT_CORE_ASSERT(internalFormat & dataFormat, "Format not suported");
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
@@ -44,10 +64,19 @@ namespace Raptor {
 
 		stbi_image_free(data);
 	}
+
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
 		glDeleteTextures(1, &m_RendererID);
 	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpc = m_DataFormat == GL_RGBA ? 4 : 3;
+		RT_CORE_ASSERT(size == m_Width * m_Height * bpc, "Data must be entire texture");
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
+	}
+
 	void OpenGLTexture2D::Bind(uint32_t sloat) const
 	{
 		glBindTextureUnit(sloat, m_RendererID);
