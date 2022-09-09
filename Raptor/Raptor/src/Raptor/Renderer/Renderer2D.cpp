ca654e93d7a50b26fce368544494d21d8dd16503
seneshMH <senesh1senesh@gmail.com>
2022-09-10 01:42:53 +0530

    Renderer Stats and Batch Improvements

--- Raptor/Raptor/src/Raptor/Renderer/Renderer2D.cpp
+++ Raptor/Raptor/src/Raptor/Renderer/Renderer2D.cpp
@@ -20,9 +20,9 @@ namespace Raptor {
 
 	struct Renderer2DData
 	{
-		const uint32_t MaxQuads = 10000;
-		const uint32_t MaxVertices = MaxQuads * 4;
-		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxQuads = 20000;
+		static const uint32_t MaxVertices = MaxQuads * 4;
+		static const uint32_t MaxIndices = MaxQuads * 6;
 		static const uint32_t MaxTextureSlots = 32; //TODO: Fix
 
 		Ref<VertexArray> QuadVertexArray;
@@ -38,6 +38,8 @@ namespace Raptor {
 		uint32_t TextureSlotIndex = 1; //0 is white texture
 
 		glm::vec4 QuadVertexPosition[4];
+
+		Renderer2D::Statistics stats;
 	};
 
 	
@@ -143,6 +145,16 @@ namespace Raptor {
 		}
 
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray,s_Data.QuadIndexCount);
+		s_Data.stats.DrawCalls++;
+	}
+
+	void Renderer2D::FlushAndRest()
+	{
+		EndScene();
+
+		s_Data.QuadIndexCount = 0;
+		s_Data.QuadVertexBufferptr = s_Data.QuadVertexBufferBase;
+		s_Data.TextureSlotIndex = 1;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -153,6 +165,11 @@ namespace Raptor {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
 		RT_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+		{
+			FlushAndRest();
+		}
 
 		const float texIndex = 0.0f; //White texture
 		const float tilingFactor = 1.0f;
@@ -189,6 +206,8 @@ namespace Raptor {
 		s_Data.QuadVertexBufferptr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D> texture, float tililingFactor, const glm::vec4& tintColor)
@@ -199,6 +218,11 @@ namespace Raptor {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D> texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		RT_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+		{
+			FlushAndRest();
+		}
 
 		constexpr glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f };
 		
@@ -251,6 +275,8 @@ namespace Raptor {
 		s_Data.QuadVertexBufferptr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
@@ -262,6 +288,10 @@ namespace Raptor {
 	{
 		RT_PROFILE_FUNCTION();
 
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+		{
+			FlushAndRest();
+		}
 
 		const float texIndex = 0.0f; //White texture
 		const float tilingFactor = 1.0f;
@@ -299,6 +329,8 @@ namespace Raptor {
 		s_Data.QuadVertexBufferptr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D> texture, float tilingFactor, const glm::vec4& tintColor)
@@ -309,6 +341,11 @@ namespace Raptor {
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D> texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		RT_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+		{
+			FlushAndRest();
+		}
 
 		constexpr glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f };
 
@@ -362,6 +399,18 @@ namespace Raptor {
 		s_Data.QuadVertexBufferptr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.stats.QuadCount++;
+	}
+
+	void Renderer2D::ResetStats()
+	{
+		memset(&s_Data.stats, 0, sizeof(Statistics));
+	}
+
+	Renderer2D::Statistics Renderer2D::GetStats()
+	{
+		return s_Data.stats;
 	}
 
 }
