01c05836fd83e75cce7e9660604761b9a5506c06
seneshMH <senesh1senesh@gmail.com>
2022-09-15 12:12:16 +0530

    Add EventCategoryMouseButton to MouseButtonEvent

--- Raptor/Raptor/src/Raptor/Event/MouseEvent.h
+++ Raptor/Raptor/src/Raptor/Event/MouseEvent.h
@@ -55,7 +55,7 @@ namespace Raptor {
 	public:
 		inline int GetMouseButton() const { return m_Button; }
 
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
 	protected:
 		MouseButtonEvent(int button)
 			: m_Button(button) {}
