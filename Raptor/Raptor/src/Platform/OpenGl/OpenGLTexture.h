54fea383d68f833f7c84f7b1bc4d908098a664a3
seneshMH <senesh1senesh@gmail.com>
2023-03-17 23:47:28 +0530

    Update the texture comparison method

--- Raptor/Raptor/src/Platform/OpenGl/OpenGLTexture.h
+++ Raptor/Raptor/src/Platform/OpenGl/OpenGLTexture.h
@@ -20,7 +20,7 @@ namespace Raptor {
 
 		virtual bool operator==(const Texture& other) const override
 		{
-			return m_RendererID == ((OpenGLTexture2D&)other).m_RendererID;
+			return m_RendererID == other.GetRendererID();
 		}
 
 	private:
