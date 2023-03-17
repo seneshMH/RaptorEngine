5201627164c4803872823fb907ee798c0d1225ae
seneshMH <senesh1senesh@gmail.com>
2023-03-18 00:52:47 +0530

    Texture and Tiling Factor in SpriteRendererComponent is now serialized

--- Raptor/Raptor/src/Raptor/Scene/SceneSrializer.cpp
+++ Raptor/Raptor/src/Raptor/Scene/SceneSrializer.cpp
@@ -200,6 +200,10 @@ namespace Raptor {
 
 			auto& spriteRendererComponent = entity.GetComponent<SpriteRendererComponent>();
 			out << YAML::Key << "Color" << YAML::Value << spriteRendererComponent.Color;
+			if (spriteRendererComponent.Texture)
+				out << YAML::Key << "TexturePath" << YAML::Value << spriteRendererComponent.Texture->GetPath();
+
+			out << YAML::Key << "TilingFactor" << YAML::Value << spriteRendererComponent.TilingFactor;
 
 			out << YAML::EndMap;//SpriteRendererComponent
 		}
@@ -299,7 +303,17 @@ namespace Raptor {
 		std::stringstream strStream;
 		strStream << stream.rdbuf();
 
-		YAML::Node data = YAML::Load(strStream.str());
+		YAML::Node data;
+		try
+		{
+			data = YAML::LoadFile(filepath);
+		}
+		catch (YAML::ParserException e)
+		{
+			RT_CORE_ERROR("Failed to load .raptor file '{0}'\n     {1}", filepath, e.what());
+			return false;
+		}
+
 		if (!data["Scene"])
 			return false;
 
@@ -357,6 +371,12 @@ namespace Raptor {
 				{
 					auto& src = desirializedEntity.AddComponent<SpriteRendererComponent>();
 					src.Color = spriteRendererComponent["Color"].as<glm::vec4>();
+
+					if (spriteRendererComponent["TexturePath"])
+						src.Texture = Texture2D::Create(spriteRendererComponent["TexturePath"].as<std::string>());
+
+					if (spriteRendererComponent["TilingFactor"])
+						src.TilingFactor = spriteRendererComponent["TilingFactor"].as<float>();
 				}
 
 				auto circleRendererComponent = entity["CircleRendererComponent"];
