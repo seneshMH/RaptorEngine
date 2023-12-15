5b2d88a03f280e3a174fdceaa99d0ef8fb5b1a09
senesh <senesh1senesh@gmail.com>
2023-12-15 22:12:10 +0530

    adding icon to editor

--- Raptor/Raptor/src/Platform/Windows/WindowsWindow.cpp
+++ Raptor/Raptor/src/Platform/Windows/WindowsWindow.cpp
@@ -9,6 +9,8 @@
 
 #include "Platform/OpenGl/OpenGlContext.h"
 
+#include <stb_image.h>
+
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 #include <Windows.h>
@@ -68,8 +70,13 @@ namespace Raptor {
 
 
 			m_Window = glfwCreateWindow((int)props.width, (int)props.height, m_Data.Title.c_str(), nullptr, nullptr);
-			auto hwnd = glfwGetWin32Window(m_Window);
 
+			GLFWimage images[1];
+			images[0].pixels = stbi_load("assets/icons/icon.png", &images[0].width, &images[0].height, 0, 4); //rgba channels 
+			glfwSetWindowIcon(m_Window, 1, images);
+			stbi_image_free(images[0].pixels);
+			
+			auto hwnd = glfwGetWin32Window(m_Window);
 			BOOL dark = TRUE;
 			DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark, sizeof(dark));
 
